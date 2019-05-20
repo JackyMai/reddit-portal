4647189031e5c60ee31f74c52b41e9267c011309
Jacky Mai <jacky.jy.mai@gmail.com>
2019-05-21 00:18:57 +1200

    Add the ability to specify exact subreddit name

--- main.js
+++ main.js
@@ -3,8 +3,32 @@ const baseURL = 'https://www.reddit.com';
 const autocompleteAPI = 'search_reddit_names';
 
 async function getSubredditSuggestions(text) {
-  const response = await fetch(`${baseURL}/api/${autocompleteAPI}.json?query=${text}`);
+  let isExact = false;
+
+  if (text[0] === '!') {
+    text = text.substring(1);
+    isExact = true;
+  }
+
+  if (text[0] === '"' && text[text.length - 1] === '"') {
+    text = text.substring(1, text.length - 1);
+    isExact = true;
+  }
+
+  const options = {
+    exact: isExact,
+    include_over_18: false,
+    include_advertisable: false
+  };
+
+  const params = Object.keys(options).reduce(
+    (accumulator, key) => `${accumulator}&${key}=${options[key]}`,
+    `query=${text}`
+  );
+
+  const response = await fetch(`${baseURL}/api/${autocompleteAPI}.json?${params}`);
   const data = await response.json();
+
   return data.names.map(name => ({
     content: name,
     description: `r/${name}`
